452c4e80cfdf2f25323fac3cecc831f45cb0fe12
ababu alemu <ababualemu123@gmail.com>
2022-08-18 15:59:56 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <section className="landing">
+        <h1 id="header">Hey! I am<br/> Ababu Alemu</h1>
+      </section>
+    </main>
+  );
+}
+
+export default App;
